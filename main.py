cb06a285187f1bd65a1e41e09d31050bf9bbba03
Saqib Rashid <saqibrashid446@gmail.com>
2026-07-14 16:07:10 +0500

    frontend template and add fastapi

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import random
-import string 
-password = ""
+import string
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-passwordChar = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/passhome")
+def passGen():
+    password = ""
+    passwordChar = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+    password =   "".join(random.choice(passwordChar)for char in range(10))
+    return password
 
 
-password =   "".join(random.choice(passwordChar)for char in range(10))
 
-print(password)  
+
+
+
+
+ 
